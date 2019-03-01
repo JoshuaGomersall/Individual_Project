7f38f598666c08f11e934103ecfce5dc7e352b2e
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-03-01 22:32:16 +0000

    Add files via upload
    Update Added

--- src/main/java/com/qa/rest/BackStoryEndpoint.java
+++ src/main/java/com/qa/rest/BackStoryEndpoint.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -43,6 +44,15 @@ public class BackStoryEndpoint {
 		return service.deleteBackStory(id);
 	}
 
+	@Path("/updateBackStory/{id}")
+	@PUT
+	@Produces({ "application/json" })
+	public String updateBackStory (@PathParam("id") Long id , String BackStory)
+	{
+		return service.updateBackStory(BackStory, id);
+	}
+	
+	
 	public void setService(BackStoryService service) {
 		this.service = service;
 	}
